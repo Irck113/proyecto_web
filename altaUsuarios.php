ecb4800e94b04ce3c6324c52c52c6e2d83f21333
kpadillar <m_on_ka@hotmail.com>
2018-05-18 16:58:38 -0500

    ADD: Formulario alta usuarios

--- altaUsuarios.php
+++ altaUsuarios.php
@@ -13,7 +13,17 @@
                 </script>
         </head>
         <body>
-
+		<form>
+			<label>Nombre:</label><input type="text" nombre="nombre" required> <br>
+			<label>Apellido Paterno:</label><input type="text" ap_paterno="Apellido Paterno" required> <br>
+			<label>Apellido Materno:</label><input type="text" ap_materno="Apellido Materno"> <br>
+			<label>Direcci&oacuten:</label><input type="text" direccion="Direcci&oacuten" required> <br>
+			<label>Tel&eacutefono:</label><input type="text" telefono="tel&eacutefono" required> <br>
+			<label>Correo:</label> <input type="text" correo="correo" required> <br>
+			<label>Usuario:</label><input type="text" usuario="Usuario"> <br>
+			<label>Contrasena:</label><input type="text" contrasena="Contraseña"> <br>
+		<input type="submit">
+		</form>
         </body>
 </html>
 
