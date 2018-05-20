bc0332f772a2bc569c973a1df892f98f60c35093
becarios_hc <becarios.hc@gmail.com>
2018-05-20 15:08:16 -0500

    ADD: Aspecto base, agregado a los archivos.

--- altaUsuarios.php
+++ altaUsuarios.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 
 <html>
-        <head>
-                <meta charset = "utf-8"/>
-                <meta name = "viewport" content = "width=device-width"/>
-                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	<head>
+		<meta charset = "utf-8"/>
+		<meta name = "viewport" content = "width=device-width"/>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 		<link rel = "stylesheet" href = "css/bootstrap.css"/>
 		<link rel = "stylesheet" href = "css/main.css"/>
-        </head>
-        <body>
+	</head>
+	<body>
 		<div class = "container pagina">
 			<header>
 				<nav class="navbar navbar-expand-lg navbar-light">
@@ -34,48 +34,48 @@
 			</header>
 			<main>
 				<form class="needs-validation">
-				<div class="form-row">
-					<div class="form-group col-4">
-						<label for="nombre">Nombre</label>
-						<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Andres Manuel" required>
+					<div class="form-row">
+						<div class="form-group col-4">
+							<label for="nombre">Nombre</label>
+							<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Andres Manuel" required>
+						</div>
+						<div class="form-group col-4">
+							<label for="aPaterno">Apellido Paterno</label>
+							<input type="text" class="form-control" id="aPaterno" name="aPaterno" placeholder="Lopez" required>
+						</div>
+						<div class="form-group col-4">
+							<label for="aMaterno">Apellido Paterno</label>
+							<input type="text" class="form-control" id="aMaterno" name="aMaterno" placeholder="Obrador" required>
+						</div>
 					</div>
-					<div class="form-group col-4">
-						<label for="aPaterno">Apellido Paterno</label>
-						<input type="text" class="form-control" id="aPaterno" name="aPaterno" placeholder="Lopez" required>
+					<div class="form-row">
+						<div class="form-group col-10">
+							<label for="direccion ">Direccion</label>
+							<input type="text" class="form-control" id="aMaterno" name="aMaterno" placeholder="Direccion" required>
+							<small class="form-text text-muted">Calle, N&uacute;mero, Colonia, Entidad federativa, Delegacion o municipio</small>
+						</div>
+						<div class="form-group col-2">
+							<label for="telefono">Telefono</label>
+							<input type="text" class="form-control" id="telefono" name="telefono" placeholder="5566778899" size="10" maxlength="10" required>
+							<small class="form-text text-muted">10 digitos</small>
+						</div>
 					</div>
-					<div class="form-group col-4">
-						<label for="aMaterno">Apellido Paterno</label>
-						<input type="text" class="form-control" id="aMaterno" name="aMaterno" placeholder="Obrador" required>
+					<div class="form-row">
+						<div class="form-group col-4">
+							<label for="correo">Correo</label>
+							<input type="email" class="form-control" id="correo" name="correo" placeholder="ejemplo@dominio.com" required>
+						</div>
+						<div class="form-group col-4">
+							<label for="usuario">Usuario</label>
+							<input type="text" class="form-control" id="usuario" name="usuario" placeholder="AMLO" required>
+							<small class="form-text text-muted">Longitud minima de 6 caracteres</small>
+						</div>
+						<div class="form-group col-4">
+							<label for="contrasenia">Contrase&ntilde;a</label>
+							<input type="password" class="form-control" id="contrasenia" name="contrasenia"  placeholder="Contrase&ntilde;a" required>
+							<small class="form-text text-muted">Longitud minima de 8 caracteres, debe incluir al menos un digito y una letra mayuscula</small>
+						</div>
 					</div>
-				</div>
-				<div class="form-row">
-					<div class="form-group col-10">
-						<label for="direccion ">Direccion</label>
-						<input type="text" class="form-control" id="aMaterno" name="aMaterno" placeholder="Direccion" required>
-						<small class="form-text text-muted">Calle, N&uacute;mero, Colonia, Entidad federativa, Delegacion o municipio</small>
-					</div>
-					<div class="form-group col-2">
-						<label for="telefono">Telefono</label>
-						<input type="text" class="form-control" id="telefono" name="telefono" placeholder="5566778899" size="10" maxlength="10" required>
-						<small class="form-text text-muted">10 digitos</small>
-					</div>
-				</div>
-				<div class="form-row">
-					<div class="form-group col-4">
-						<label for="correo">Correo</label>
-						<input type="email" class="form-control" id="correo" name="correo" placeholder="ejemplo@dominio.com" required>
-					</div>
-					<div class="form-group col-4">
-						<label for="usuario">Usuario</label>
-						<input type="text" class="form-control" id="usuario" name="usuario" placeholder="AMLO" required>
-						<small class="form-text text-muted">Longitud minima de 6 caracteres</small>
-					</div>
-					<div class="form-group col-4">
-						<label for="contrasenia">Contrase&ntilde;a</label>
-						<input type="password" class="form-control" id="contrasenia" name="contrasenia"  placeholder="Contrase&ntilde;a" required>
-						<small class="form-text text-muted">Longitud minima de 8 caracteres, debe incluir al menos un digito y una letra mayuscula</small>
-					</div>
-				</div>
 					<div class="form-check col-6">
 						<input class="form-check-input" type="radio" name="tipoUsuario" id="uVentas" value="ventas" checked>
 						<label class="form-check-label" for="uVentas">
@@ -93,24 +93,24 @@
 			</main>
 			<footer>
 				<div class="row">
-						<div class="col">
-							<p>Proyecto final de Web2.0</p>
-						</div>
-						<div class="col">
-							<p>P&aacute;gina creada por: </p>
-							<p>Ricardo Hern&aacute;ndez Garc&iacute;a</p>
-							<p>Karen Padilla Rojas</p>
-						</div>
-						<div class="col">
-							<p>Siguenos en nuestras redes sociales: </p>
-							<a href="https://www.facebook.com"><i class="fab fa-facebook-square"></i></a>
-							<a href="https://www.twitter.com"><i class="fab fa-twitter-square"></i></a>
-						</div>
+					<div class="col">
+						<p>Proyecto final de Web2.0</p>
 					</div>
+					<div class="col">
+						<p>P&aacute;gina creada por: </p>
+						<p>Ricardo Hern&aacute;ndez Garc&iacute;a</p>
+						<p>Karen Padilla Rojas</p>
+					</div>
+					<div class="col">
+						<p>Siguenos en nuestras redes sociales: </p>
+						<a href="https://www.facebook.com"><i class="fab fa-facebook-square"></i></a>
+						<a href="https://www.twitter.com"><i class="fab fa-twitter-square"></i></a>
+					</div>
+				</div>
 			</footer>
 		</div>
 		<script src="js/jquery-3.1.1.js"></script>
 		<script src="js/bootstrap.js"></script>
-        </body>
+	</body>
 </html>
 
